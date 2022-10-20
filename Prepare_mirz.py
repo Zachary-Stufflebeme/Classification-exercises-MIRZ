9b3065e031a6d760486e7c2c699dc40ebdbf84bf
Zachary Stufflebeme <Stufflebeme.Zachary@gmail.com>
2022-10-20 16:36:58 -0500

    edit

--- Prepare_mirz.py
+++ Prepare_mirz.py
@@ -12,13 +12,17 @@ def prep_iris(fresh_iris):
     return iris
 
 def prep_titanic(FreshTitanic):
-    Titanic = FreshTitanic.drop(columns = {'passenger_id', 'Unnamed: 0'})
-    Titanic['encoded_class'] = FreshTitanic['class'].map({'First': 1, 'Second': 2, 'Third': 3})
+    Titanic = FreshTitanic
+    Titanic['is_male'] = FreshTitanic.sex.map({'female':0,'male':1})
+    Titanic['Cherbourg'] = Titanic.embark_town.map({'Cherbourg':1,'Southampton':0,'Queenstown':0})
+    Titanic['Southampton'] = Titanic.embark_town.map({'Southampton':1,'Queenstown':0,'Cherbourg':0})
+    Titanic['Queenstown'] = Titanic.embark_town.map({'Queenstown':1, 'Southampton':0,'Cherbourg':0})
+    Titanic = FreshTitanic.drop(columns = {'passenger_id' , 'Unnamed: 0' , 'class', 'sex','embark_town','deck','embarked'})
+    Titanic = Titanic.dropna()
     return Titanic
 
 def prep_telco_data(FreshTelco):
     df = FreshTelco.T.drop_duplicates().T
-    df = df.drop(columns = {'internet_service_type', 'customer_id','Unnamed: 0'})
     df['is_female'] = df.gender.map({'Female': 1, 'Male': 0})
     df['has_partner'] = df.partner.map({'Yes': 1, 'No': 0})
     df['has_dependents'] = df.dependents.map({'Yes': 1, 'No': 0})
@@ -32,6 +36,10 @@ def prep_telco_data(FreshTelco):
     df['has_tech_support'] = df.tech_support.map({'No' : 0, 'Yes' : 1, 'No Internet Service' : 2})
     df['can_stream_tv'] = df.streaming_tv.map({'No' : 0, 'Yes' : 1, 'No Internet Service' : 2})
     df['can_stream_movie'] = df.streaming_movies.map({'No' : 0, 'Yes' : 1, 'No Internet Service' : 2})
+    df = df.drop(columns = {'internet_service_type', 'customer_id','Unnamed: 0','contract_type', 'gender','churn','streaming_movies',
+                            'streaming_tv','tech_support', 'device_protection', 'online_backup', 'online_security', 'multiple_lines',
+                            'phone_service','partner','dependents','paperless_billing'})
+
     return df
 
 def my_train_test_split(df, target):
@@ -40,3 +48,4 @@ def my_train_test_split(df, target):
     train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
 
     return train, validate, test
+#%%
